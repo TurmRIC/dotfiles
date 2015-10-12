63e0b528d4ef17371e206be5b90e39b36e745d27
Jeremy Rose <jeremy.rose@vecima.com>
2015-10-11 22:21:55 -0600

    Fix install script

--- install.sh
+++ install.sh
@@ -18,16 +18,22 @@ install_files() {
     local dest_prefix=${3}
     for file in `ls ${src_dir}`
     do
+        if [ ${file} = "install.sh" -o ${file} = "README.md" ]
+        then
+            continue
+        fi
         if [ -f ${src_dir}/${file} ]
         then
-            echo "rm ${dest_dir}/${dest_prefix}${file} 2>/dev/null"
-            echo "ln -s ${src_dir}/${file} ${dest_dir}/${dest_prefix}${file}"
+            rm ${dest_dir}/${dest_prefix}${file} 2>/dev/null
+            ln -s ${src_dir}/${file} ${dest_dir}/${dest_prefix}${file}
         elif [ -d ${src_dir}/${file} ]
         then
-            echo "mkdir ~/${file}"
+            mkdir ${dest_dir}/${dest_prefix}${file} 2>/dev/null
             install_files "${src_dir}/${file}/" "${dest_dir}/${dest_prefix}${file}/"
         fi
     done
 }
 
-install_files "${install_dir}" "~" "."
+install_files "${install_dir}" "${HOME}" "."
+mkdir ${HOME}/.vim/swapfiles/ 2>/dev/null
+mkdir ${HOME}/.vim/backups/ 2>/dev/null
