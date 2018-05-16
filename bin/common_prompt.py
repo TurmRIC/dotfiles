4f147accb6010ce7be57b32d54e5830afe017573
Jeremy Rose <turmric@gmail.com>
2018-05-16 13:33:01 -0600

    Added a better prompt application
     - Wrote a python app to generate a prompt with a horizontal line
     dividing the commands

---
+++ bin/common_prompt.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python3
+# Not really so 'common' right now, but it is a start
+
+import os
+import re
+import subprocess
+import psutil
+import socket
+import json
+import math
+
+
+def get_work_dir(max_len):
+    work_dir = os.getcwd()
+    homedir = os.path.expanduser('~')
+    work_dir = work_dir.replace(homedir, '~', 1)
+    if len(work_dir) <= max_len:
+        return work_dir
+    rep_file = open('{}/.common_prompt.json'.format(homedir), 'r')
+    reps = json.load(rep_file)
+    for rename in reps['DirectoryAliasing']['FirstPass']:
+        work_dir = work_dir.replace(rename['LongName'], rename['ShortName'])
+    if len(work_dir) <= max_len:
+        return work_dir
+    for regs in reps['DirectoryAliasing']['SecondPass']:
+        work_dir = re.sub(regs['RegEx'], regs['Replacement'], work_dir)
+    if len(work_dir) <= max_len:
+        return work_dir
+    # split available characters from front and back
+    split_chars = math.floor((max_len - 3)/2)
+    work_dir = work_dir[:split_chars]+'...'+work_dir[-split_chars:]
+    print(len(work_dir))
+    return work_dir
+
+
+def main():
+    colour_string = '%F{{{}}}'
+    main_colour = colour_string.format(188)
+    background = '%K{24}'
+    reset_background = '%k'
+    reset_forground = '%f'
+    host_colour = colour_string.format(208)
+    user_colour = colour_string.format(154)
+    work_dir_colour = colour_string.format(214)
+    prompt_char = '▶'
+    prompt_colour = '%(?.%F{2}.%F{1})'
+    fill_char = '─'
+    left_char = '┤'
+    right_char = '├'
+
+    prompt = ''
+    rows, columns = subprocess.check_output(['stty', 'size']).decode().split()
+    rows = int(rows)
+    columns = int(columns)
+    tmux = os.getenv('TMUX')
+    ppid = os.getppid()
+    parent = psutil.Process(ppid)
+    prompt += '{}{}'.format(main_colour, background)
+    prompt_len = 0
+    if tmux is None:
+        non_local_format = '{}{} {}{}{}@{}{}{} {}'
+        user = os.getenv('USER')
+        hostname = socket.gethostname()
+        prompt += non_local_format.format(
+                fill_char,
+                left_char,
+                user_colour,
+                user,
+                main_colour,
+                host_colour,
+                hostname,
+                main_colour,
+                right_char)
+        prompt_len += len(user) + len(hostname)
+        prompt_len += len(non_local_format.format(
+            fill_char, left_char, '', '', '', '', '', '', right_char))
+    work_dir_fmt = '{} {}{}{} {}'
+    work_fmt_len = len(work_dir_fmt.format(left_char, '', '', '', right_char))
+    max_work_dir_len = (columns
+                        - prompt_len
+                        - work_fmt_len
+                        - 2)
+    work_dir = get_work_dir(max_work_dir_len)
+    cols_remaining = (columns - len(work_dir) - work_fmt_len)
+    half_cols = math.floor(cols_remaining / 2)
+#    print('columns: {}'.format(columns))
+#    print('prompt_len: {}'.format(prompt_len))
+#    print('work_fmt_len: {}'.format(work_fmt_len))
+#    print('max_work_dir_len: {}'.format(max_work_dir_len))
+#    print('work_dir_len: {}'.format(len(work_dir)))
+#    print('cols_remaining: {}'.format(cols_remaining))
+#    print('half_cols: {}'.format(half_cols))
+    if (half_cols > prompt_len):
+        prompt += fill_char * (half_cols - prompt_len)
+    else:
+        prompt += fill_char
+    prompt += work_dir_fmt.format(
+            left_char, work_dir_colour, work_dir, main_colour, right_char)
+    remainder = half_cols + cols_remaining % 2
+    if (half_cols < prompt_len):
+        remainder += (half_cols - prompt_len - 1)
+    prompt += fill_char * remainder
+    prompt += fill_char * 2
+    prompt += '{}{}{}{} '.format(
+            prompt_colour,
+            prompt_char,
+            reset_background,
+            reset_forground)
+    print(prompt)
+
+
+if __name__ == '__main__':
+    main()
--- bin/short_pwd.py
+++ bin/short_pwd.py
@@ -1,26 +1,7 @@
 #! /usr/bin/env python3
 
-import os
-import re
+import common_prompt
 
-pwd = os.getcwd()
-homedir = os.path.expanduser('~')
-pwd = pwd.replace(homedir, '~', 1)
-pwd = pwd.replace('4D_Tintagel', '4D')
-pwd = pwd.replace('4D_Overlay', '4D_O')
-pwd = pwd.replace('4P_Galiano', '4P')
-pwd = pwd.replace('4P2_Parker', '4P2')
-pwd = pwd.replace('4P_Overlay', '4P_O')
-pwd = pwd.replace('target_src', 't_s')
-pwd = pwd.replace('build_tools', 'b_t')
-pwd = pwd.replace('test_tools', 't_t')
-pwd = pwd.replace('ROOB_WORKING', 'ROOB')
-pwd = pwd.replace('RF_WORKING', 'RF')
-if len(pwd) > 40:
-    pwd = re.sub(r'\bbin\b', 'b', pwd)
-    pwd = re.sub(r'\blib\b', 'l', pwd)
-    pwd = re.sub(r'\bsrc\b', 's', pwd)
-    pwd = re.sub(r'\btest\b', 't', pwd)
-    if len(pwd) > 40:
-        pwd = pwd[:10]+'...'+pwd[-27:] # first 10 chars+last 27 chars
-print(pwd)
+work_dir = common_prompt.get_work_dir(40)
+
+print(work_dir)
